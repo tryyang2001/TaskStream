18fbe344f867695c45ce7d1dcc1129e32704d9cd
e0550472@u.nus.edu <tryyang2001@gmail.com>
2021-09-17 17:17:01 +0800

    Add method for printDataWithStreams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -46,6 +46,12 @@ public class Main {
         }
     }
 
+    public static void printDataWithStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using stream");
+        tasks.stream() //convert to stream
+                .forEach(System.out::println); //for each element, print out the data
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
